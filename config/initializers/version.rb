9555ef7d8c9aa2ad51d4eac9a31ee2dd40187e54
Asya Nickolskaya <nickolskaya@gmail.com>
2025-02-23 16:22:31 +0300

    fix: version

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,9 +1,7 @@
 module TestGuruVersion
-  MAJOR = 1
+  MAJOR = 0
   MINOR = 0
   PATCH = 0
-  PRE_RELEASE = nil
 
   VERSION = [MAJOR, MINOR, PATCH].join('.')
-  VERSION += "-#{PRE_RELEASE}" if PRE_RELEASE
 end
