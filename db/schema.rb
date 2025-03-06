d6f13890bbb313eb4001185b7bebec63bc0640b2
Asya Nickolskaya <nickolskaya@gmail.com>
2025-03-06 22:55:27 +0300

    feat: add AddCorrectToAnswers

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_06_194916) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_06_195250) do
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.integer "question_id", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "correct"
   end
 
   create_table "categories", force: :cascade do |t|
