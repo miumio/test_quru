5b666bd85f94901d0f7e03459fe778202ab07253
Asya Nickolskaya <nickolskaya@gmail.com>
2025-03-25 23:01:10 +0200

    fix rubocop errs

--- db/migrate/20250326165731_create_tests.rb
+++ db/migrate/20250326165731_create_tests.rb
@@ -1,9 +1,9 @@
 class CreateTests < ActiveRecord::Migration[7.2]
   def change
     create_table :tests do |t|
-      t.string :title, null: false 
+      t.string :title, null: false
       t.integer :level, default: 1, null: false
-      
+
       t.references :category, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
