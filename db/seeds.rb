1ae56c88bf4c2231868c1b0ad85c5418a2b60ad1
Asya Nickolskaya <nickolskaya@gmail.com>
2025-03-31 16:32:22 +0200

    fix: rubocop errs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 puts "Clearing database..."
-[Answer, Question, Test, Category, User].each(&:destroy_all)
+[ Answer, Question, Test, Category, User ].each(&:destroy_all)
 
 puts "Creating categories..."
 categories = {
@@ -8,7 +8,7 @@ categories = {
   db: "Базы данных"
 }
 
-users_names = ["John Snow", "Tyrion Lannister", "Joffrey Baratheon"]
+users_names = [ "John Snow", "Tyrion Lannister", "Joffrey Baratheon" ]
 
 categories_records = Category.create!(
   categories.values.map { |title| { title: title } }
@@ -24,7 +24,7 @@ test_data = [
   { title: "Основы Ruby", level: 1, category_id: Category.find_by(title: categories[:programming]).id, author_id: users.first.id },
   { title: "SQL: базовые запросы", level: 2, category_id: Category.find_by(title: categories[:db]).id, author_id: users.last.id },
   { title: "HTML и CSS", level: 1, category_id: Category.find_by(title: categories[:web]).id, author_id: users.last.id },
-  { title: "Rails для начинающих", level: 2, category_id: Category.find_by(title: categories[:web]).id, author_id: users.first.id },
+  { title: "Rails для начинающих", level: 2, category_id: Category.find_by(title: categories[:web]).id, author_id: users.first.id }
 ]
 
 tests = Test.create!(test_data)
@@ -32,19 +32,19 @@ tests = Test.create!(test_data)
 puts "Creating questions & answers..."
 questions_answers = {
   "Основы Ruby" => [
-    { body: "Как объявить символ в Ruby?", answers: [":symbol", "\"symbol\"", "@symbol", "&symbol"], correct: 0 },
-    { body: "Какой метод используется для итерации по массиву в Ruby?", answers: ["each", "forEach", "loop", "iterate"], correct: 0 },
-    { body: "Что такое блоки (blocks) в Ruby?", answers: ["Анонимные функции, которые можно передавать методам", "Объекты класса Block", "Участки кода внутри условий if-else", "Инициализаторы переменных"], correct: 0 }
+    { body: "Как объявить символ в Ruby?", answers: [ ":symbol", "\"symbol\"", "@symbol", "&symbol" ], correct: 0 },
+    { body: "Какой метод используется для итерации по массиву в Ruby?", answers: [ "each", "forEach", "loop", "iterate" ], correct: 0 },
+    { body: "Что такое блоки (blocks) в Ruby?", answers: [ "Анонимные функции, которые можно передавать методам", "Объекты класса Block", "Участки кода внутри условий if-else", "Инициализаторы переменных" ], correct: 0 }
   ],
   "SQL: базовые запросы" => [
-    { body: "Какой запрос используется для выборки данных из таблицы?", answers: ["SELECT", "EXTRACT", "OBTAIN", "GET"], correct: 0 },
-    { body: "Как объединить две таблицы в SQL?", answers: ["JOIN", "MERGE", "COMBINE", "UNION"], correct: 0 },
-    { body: "Для чего используется оператор WHERE?", answers: ["Для фильтрации результатов запроса", "Для сортировки результатов запроса", "Для группировки результатов запроса", "Для объединения таблиц"], correct: 0 }
+    { body: "Какой запрос используется для выборки данных из таблицы?", answers: [ "SELECT", "EXTRACT", "OBTAIN", "GET" ], correct: 0 },
+    { body: "Как объединить две таблицы в SQL?", answers: [ "JOIN", "MERGE", "COMBINE", "UNION" ], correct: 0 },
+    { body: "Для чего используется оператор WHERE?", answers: [ "Для фильтрации результатов запроса", "Для сортировки результатов запроса", "Для группировки результатов запроса", "Для объединения таблиц" ], correct: 0 }
   ],
   "HTML и CSS" => [
-    { body: "Какой тег используется для создания заголовка первого уровня?", answers: ["<h1>", "<header>", "<title>", "<heading>"], correct: 0 },
-    { body: "Как указать внешний CSS-файл в HTML?", answers: ["<link rel=\"stylesheet\" href=\"styles.css\">", "<style src=\"styles.css\">", "<css import=\"styles.css\">", "<script type=\"css\" src=\"styles.css\">"], correct: 0 },
-    { body: "Что такое CSS-селектор?", answers: ["Шаблон для выбора элементов, к которым будут применяться стили", "Функция JavaScript для выбора элементов DOM", "Специальный тег HTML для определения стилей", "Атрибут для указания стилей непосредственно в HTML"], correct: 0 }
+    { body: "Какой тег используется для создания заголовка первого уровня?", answers: [ "<h1>", "<header>", "<title>", "<heading>" ], correct: 0 },
+    { body: "Как указать внешний CSS-файл в HTML?", answers: [ "<link rel=\"stylesheet\" href=\"styles.css\">", "<style src=\"styles.css\">", "<css import=\"styles.css\">", "<script type=\"css\" src=\"styles.css\">" ], correct: 0 },
+    { body: "Что такое CSS-селектор?", answers: [ "Шаблон для выбора элементов, к которым будут применяться стили", "Функция JavaScript для выбора элементов DOM", "Специальный тег HTML для определения стилей", "Атрибут для указания стилей непосредственно в HTML" ], correct: 0 }
   ]
 }
 
