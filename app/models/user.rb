32e58831462934403470aa2ca786c5d8a54bfc48
Asya Nickolskaya <nickolskaya@gmail.com>
2025-03-31 23:20:17 +0200

    Feat: add test desc_list_by_category

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     Test.joins('JOIN "test_results" ON "tests"."id" = "test_results"."test_id"')
         .where(level: level)
         .where(test_results: { user_id: self.id })
-        .pluck(:title)  end
+        .pluck(:title)
+    end
 end
