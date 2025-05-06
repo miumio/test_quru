76ec4dde4a2138f14fa20bdca2a0088147204038
Asya Nickolskaya <nickolskaya@gmail.com>
2025-05-06 19:12:09 +0300

    Feat: associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,13 @@
 class Test < ApplicationRecord
-  def self.desc_list_by_category(category_title)
-    joins('JOIN "categories" ON "tests"."category_id" = "categories"."id"')
+  belongs_to :category
+  belongs_to :user, class_name: "User", foreign_key: "author_id"
+
+  has_many :questions, dependent: :destroy
+  has_many :test_results, dependent: :destroy
+  has_many :passed_tests, through: :test_results, source: :user
+
+  def self.category_titles_desc(category_title)
+    joins(:category)
       .where(categories: { title: category_title })
       .order(created_at: :desc)
       .pluck(:title)
